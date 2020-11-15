d095efef598f68910706a544f8d7a12e3d106bc2
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-11-15 04:12:12 +0000

    Commit

--- Form/ProfileFormType.php
+++ Form/ProfileFormType.php
@@ -19,25 +19,50 @@ class ProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        
         $builder
-        ->add('apiToken', HiddenType::class)
-        ->add('firstName', TextType::class, array('label' => 'registration.firstName', 'translation_domain' => 'IAUsersBundle'))
-        ->add('lastName', TextType::class, array('label' => 'registration.lastName', 'translation_domain' => 'IAUsersBundle'))
-        ->add('country', CountryType::class, array('label' => 'registration.yourCountry', 'translation_domain' => 'IAUsersBundle'))
-        ->add('birthday', BirthdayType::class, array('label' => 'registration.yourBirthday', 'translation_domain' => 'IAUsersBundle', 'widget'=>'single_text'))
-        ->add('occupation', TextType::class, array('label' => 'registration.occupation', 'translation_domain' => 'IAUsersBundle', 'required' => false))
-        ->add('mobile', TelType::class, array('label' => 'registration.mobile', 'translation_domain' => 'IAUsersBundle', 'required' => false))
-        ->add('website', UrlType::class, array('label' => 'registration.websiteUrl', 'translation_domain' => 'IAUsersBundle', 'required' => false))
-        
-        ->add('btnSave', SubmitType::class, array('label' => 'form.save', 'translation_domain' => 'IAUsersBundle'))
-        ->add('btnCancel', ButtonType::class, array('label' => 'form.cancel', 'translation_domain' => 'IAUsersBundle'))
+            ->add( 'apiToken', HiddenType::class )
+            ->add( 'firstName', TextType::class, [
+                'label'                 => 'registration.firstName',
+                'translation_domain'    => 'VSUsersBundle'
+            ])
+            ->add( 'lastName', TextType::class, [
+                'label'                 => 'registration.lastName',
+                'translation_domain'    => 'VSUsersBundle'
+            ])
+            ->add( 'country', CountryType::class, [
+                'label'                 => 'registration.yourCountry',
+                'translation_domain'    => 'VSUsersBundle'   
+            ])
+            ->add( 'birthday', BirthdayType::class, [
+                'label'                 => 'registration.yourBirthday',
+                'translation_domain'    => 'VSUsersBundle',
+                'widget'                => 'single_text'
+            ])
+            ->add( 'occupation', TextType::class, [
+                'label'                 => 'registration.occupation',
+                'translation_domain'    => 'VSUsersBundle',
+                'required'              => false
+            ])
+            ->add( 'mobile', TelType::class, [
+                'label'                 => 'registration.mobile',
+                'translation_domain'    => 'VSUsersBundle',
+                 'required'             => false
+            ])
+            ->add( 'website', UrlType::class, [
+                'label'                 => 'registration.websiteUrl',
+                'translation_domain'    => 'VSUsersBundle',
+                'required'              => false
+            ])
+            
+            ->add( 'btnSave', SubmitType::class, [
+                'label'                 => 'form.save',
+                'translation_domain'    => 'VSUsersBundle'
+            ])
+            ->add( 'btnCancel', ButtonType::class, [
+                'label'                 => 'form.cancel',
+                'translation_domain'    => 'VSUsersBundle'
+            ])
         ;
-    }
-    
-    public function getName()
-    {
-        return 'userInfo';
     }
     
     public function configureOptions(OptionsResolver $resolver)
