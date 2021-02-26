ec042f93ed7798cce1f8c9dafe1b6d9c2b5c2a0d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-02-26 05:16:24 +0000

    Commit

--- lib/Repository/PageCategoryRepository.php
+++ lib/Repository/PageCategoryRepository.php
@@ -4,5 +4,12 @@ use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 class PageCategoryRepository extends EntityRepository
 {
-    
+    public function find( $id, $lockMode = null, $lockVersion = null )
+    {
+        if( ! is_numeric( $id ) ) {
+            return $this->findOneBy( ['slug'=>$id] );
+        }
+        
+        return parent::find( $id, $lockMode, $lockVersion );
+    }
 }
