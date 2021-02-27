dcbec6ec94d804c7920bc5ea37fcf639f607c00a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-02-27 10:28:46 +0000

    Commit

---
+++ lib/Security/LogoutSuccessHandler.php
@@ -0,0 +1,31 @@
+<?php namespace Symfony\Component\Security\Http\Logout;
+
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\HttpUtils;
+
+/*
+ * https://chrisguitarguy.com/2019/08/06/symfony-logout-handlers-vs-logout-success-handlers/
+ */
+class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
+{
+    protected $httpUtils;
+    protected $targetUrl;
+    
+    public function __construct( HttpUtils $httpUtils, string $targetUrl = '/' )
+    {
+        $this->httpUtils = $httpUtils;
+        $this->targetUrl = $targetUrl;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function onLogoutSuccess( Request $request )
+    {
+        $response   = $this->httpUtils->createRedirectResponse( $request, $this->targetUrl );
+        $response->headers->clearCookie( 'api_token', '/', null );
+        
+        return $response;
+    }
+}
