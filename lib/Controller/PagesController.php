aa44af6faef4c6c1f43b01333b89c00d5e7c63e8
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-02-28 07:00:04 +0000

    Commit

--- lib/Controller/PagesController.php
+++ lib/Controller/PagesController.php
@@ -4,6 +4,11 @@ use VS\ApplicationBundle\Controller\AbstractCrudController;
 
 class PagesController extends AbstractCrudController
 {
-    
+    protected function customData(): array
+    {
+        return [
+            'categories'    => $this->get( 'vs_cms.repository.page_categories' )->findAll()
+        ];
+    }
 }
     
