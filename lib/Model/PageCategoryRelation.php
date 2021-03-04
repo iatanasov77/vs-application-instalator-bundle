90f3529fe02f4163f37b629e41a807b6b5a00b37
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-03-04 08:11:17 +0000

    Create PageCategoryRelation model.

--- lib/Model/PageCategoryRelation.php
+++ lib/Model/PageCategoryRelation.php
@@ -2,9 +2,17 @@
 
 class PageCategoryRelation implements PageCategoryRelationInterface
 {
+    /** @var integer */
+    protected $id;
+    
     protected $page;
     
     protected $category;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
     
     public function getPage() : PageInterface
     {
