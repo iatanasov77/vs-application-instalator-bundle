17e4db00fc2299eabff6723b1a7a1b230c8ff20b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-03-04 15:05:07 +0000

    Fix PageCategoryForm to show/edit parent combo.

--- lib/Form/PageCategoryForm.php
+++ lib/Form/PageCategoryForm.php
@@ -1,6 +1,7 @@
 <?php namespace VS\CmsBundle\Form;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\FormBuilderInterface;
 
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -15,15 +16,20 @@ class PageCategoryForm extends AbstractResourceType
 {
     protected $categoryClass;
     
-    public function __construct( string $dataClass )
+    protected $repository;
+    
+    public function __construct( string $dataClass, EntityRepository $repository )
     {
         parent::__construct( $dataClass );
         
-        $this->categoryClass = $dataClass;
+        $this->categoryClass    = $dataClass;
+        $this->repository       = $repository;
     }
     
     public function buildForm( FormBuilderInterface $builder, array $options )
     {
+        $category   = $options['data'];
+        
         $builder
             ->add( 'locale', ChoiceType::class, [
                 'label'     => 'Locale',
@@ -38,7 +44,19 @@ class PageCategoryForm extends AbstractResourceType
                 'label'         => 'Parent Category',
                 
                 'class'         => $this->categoryClass,
+                'query_builder' => function ( EntityRepository $er ) use ( $category )
+                {
+                    $categoryId = $category ? $category->getId() : null;
+                    
+                    return $er->createQueryBuilder( 'pc' )
+                            ->where( 'pc.id != :id' )
+                            ->setParameter( 'id', $categoryId )
+                        ;
+                },
                 'choice_label'  => 'name',
+                
+                'required'      => false,
+                'placeholder'   => '-- Parent Category --',
             ])
 
             ->add( 'btnSave', SubmitType::class, ['label' => 'Save'] )
