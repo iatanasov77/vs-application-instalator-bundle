f5069b080fe03194326205d30d77ae0e9209006f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-15 08:32:46 +0000

    Add GetImageController.

---
+++ lib/Controller/GetImageController.php
@@ -0,0 +1,31 @@
+<?php  namespace VS\CmsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class GetImageController extends Controller
+{
+    public function getFile( $file, Request $request )
+    {
+        $filter = $request->query->get( 'filter' );
+        
+        if ( $filter ) {
+            /** @var \Liip\ImagineBundle\Binary\Loader\FileSystemLoader $loader */
+            //$loader = $this->get( 'liip_imagine.binary.loader.filesystem' );
+            $loader = $this->get( 'liip_imagine.binary.loader.default' );
+            
+            /** @var \Liip\ImagineBundle\Imagine\Filter\FilterManager $filterManager */
+            $filterManager = $this->get( 'liip_imagine.filter.manager' );
+            
+            $filteredImageBinary = $filterManager->applyFilter( $loader->find( $file ), $filter );
+            
+            return new Response( $filteredImageBinary->getContent(), 200, [
+                'Content-Type' => $filteredImageBinary->getMimeType(),
+            ]);
+        } else {
+            return new BinaryFileResponse( $this->getParameter( 'kernel.project_dir' ) . '/' . $file );
+        }
+    }
+}
