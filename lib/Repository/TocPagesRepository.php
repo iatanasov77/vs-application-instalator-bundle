5329cafc95e998a58c989e9a76802f14cb332fbe
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-07-02 14:02:48 +0000

    Not Use Sylius RepositoryInterface for now.

--- lib/Repository/TocPagesRepository.php
+++ lib/Repository/TocPagesRepository.php
@@ -3,7 +3,18 @@
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
-class TocPagesRepository extends NestedTreeRepository implements RepositoryInterface
+/**
+ * 
+ * Sylius RepositoryInterface contains 3 abstract methods and must therefore be declared abstract 
+ * or implement the remaining methods 
+ * (
+ *      Sylius\Component\Resource\Repository\RepositoryInterface::createPaginator, 
+ *      Sylius\Component\Resource\Repository\RepositoryInterface::add, 
+ *      Sylius\Component\Resource\Repository\RepositoryInterface::remove
+ * )
+ *
+ */
+class TocPagesRepository extends NestedTreeRepository // implements RepositoryInterface
 {
     
 }
