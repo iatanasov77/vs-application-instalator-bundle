df56b07b8193e382bb0e23cafa0acaa375b0f035
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-10-20 14:30:59 +0000

    Squashed 'src/Vankosoft/CmsBundle/' content from commit a197dd4
    git-subtree-dir: src/Vankosoft/CmsBundle
    git-subtree-split: a197dd4c01de0a4bcf5f705d1ff95cbc9d391673

---
+++ lib/Model/MultiPageTocInterface.php
@@ -0,0 +1,12 @@
+<?php namespace VS\CmsBundle\Model;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface MultiPageTocInterface extends ResourceInterface
+{
+    public function getTocTitle(): string;
+    
+    public function getMainPage(): ?PageInterface;
+    
+    public function getTocRootPage(): ?TocPageInterface;
+}
