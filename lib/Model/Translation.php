da8f9c7a4089e6f9decc4ff0cc2b6772d52e0207
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-10-21 06:10:12 +0000

    Squashed 'src/Vankosoft/ApplicationBundle/' content from commit a7ad12c
    git-subtree-dir: src/Vankosoft/ApplicationBundle
    git-subtree-split: a7ad12cde3d011b2d7f463957c8bfb7d4eb58392

---
+++ lib/Model/Translation.php
@@ -0,0 +1,9 @@
+<?php namespace VS\ApplicationBundle\Model;
+
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation;
+use VS\ApplicationBundle\Model\Interfaces\TranslationInterface;
+
+class Translation extends AbstractTranslation implements TranslationInterface
+{
+    
+}
