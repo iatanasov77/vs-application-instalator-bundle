cdcbd325b9f7221f41b2d811d1f5bbd449ec1f32
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-11 19:37:23 +0000

    Many Fixes And New Doctrine Migration.

--- src/Vankosoft/CmsBundle/Component/Generator/ImagePathGeneratorInterface.php
+++ src/Vankosoft/CmsBundle/Component/Generator/ImagePathGeneratorInterface.php
@@ -1,19 +1,6 @@
-<?php
+<?php namespace VS\CmsBundle\Component\Generator;
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Paweł Jędrzejewski
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-declare(strict_types=1);
-
-namespace Sylius\Component\Core\Generator;
-
-use Sylius\Component\Core\Model\ImageInterface;
+use VS\CmsBundle\Model\ImageInterface;
 
 interface ImagePathGeneratorInterface
 {
--- src/Vankosoft/CmsBundle/Component/Generator/UploadedImagePathGenerator.php
+++ src/Vankosoft/CmsBundle/Component/Generator/UploadedImagePathGenerator.php
@@ -1,35 +1,23 @@
-<?php
+<?php namespace VS\CmsBundle\Component\Generator;
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Paweł Jędrzejewski
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-declare(strict_types=1);
-
-namespace Sylius\Component\Core\Generator;
-
-use Sylius\Component\Core\Model\ImageInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use VS\CmsBundle\Model\ImageInterface;
 
 final class UploadedImagePathGenerator implements ImagePathGeneratorInterface
 {
-    public function generate(ImageInterface $image): string
+    public function generate( ImageInterface $image ): string
     {
         /** @var UploadedFile $file */
-        $file = $image->getFile();
+        $file   = $image->getFile();
+        
+        $hash   = bin2hex( random_bytes( 16 ) );
 
-        $hash = bin2hex(random_bytes(16));
-
-        return $this->expandPath($hash . '.' . $file->guessExtension());
+        return $this->expandPath( $hash . '.' . $file->guessExtension() );
     }
 
     private function expandPath(string $path): string
     {
-        return sprintf('%s/%s/%s', substr($path, 0, 2), substr($path, 2, 2), substr($path, 4));
+        return sprintf( '%s/%s/%s', substr($path, 0, 2 ), substr( $path, 2, 2 ), substr( $path, 4 ) );
     }
 }
