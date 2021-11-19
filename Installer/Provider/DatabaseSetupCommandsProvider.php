490112ef924fa2e1200054d3373132a7e2248dce
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-19 06:22:11 +0000

    Fix Installer DatabaseSetupCommandsProvider.

--- Installer/Provider/DatabaseSetupCommandsProvider.php
+++ Installer/Provider/DatabaseSetupCommandsProvider.php
@@ -101,7 +101,12 @@ final class DatabaseSetupCommandsProvider implements DatabaseSetupCommandsProvid
 
     private function getDatabaseName(): string
     {
-        return $this->getEntityManager()->getConnection()->getDatabase();
+        // Doctrine\DBAL 3
+        $connectionParams   = $this->getEntityManager()->getConnection()->getParams();
+        return $connectionParams['dbname'];
+        
+        // Doctrine\DBAL 2
+        //return $this->getEntityManager()->getConnection()->getDatabase();
     }
 
     private function getSchemaManager(): AbstractSchemaManager
