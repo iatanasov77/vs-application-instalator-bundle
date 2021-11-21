aa891cae17f4918b2aa431dd4f4d9d71c94f5253
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-21 05:27:00 +0000

    Display UserInfo in UsersCrud.

--- src/Vankosoft/UsersBundle/Controller/UsersExtController.php
+++ src/Vankosoft/UsersBundle/Controller/UsersExtController.php
@@ -2,25 +2,80 @@
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
+use VS\CmsBundle\Component\Uploader\FileUploaderInterface;
 use VS\UsersBundle\Component\UserRole;
+use VS\UsersBundle\Form\UserInfoForm;
+use VS\UsersBundle\Model\UserInfoInterface;
 
 class UsersExtController extends AbstractController
 {
-    /* RepositoryInterface */
+    /** @var RepositoryInterface */
     protected $usersRepository;
     
-    public function __construct( RepositoryInterface $usersRepository )
-    {
-        $this->usersRepository  = $usersRepository;
+    /** @var FactoryInterface */
+    protected $userInfoFactory;
+    
+    /** @var FactoryInterface */
+    protected $avatarImageFactory;
+    
+    /** @var FileUploaderInterface */
+    protected $imageUploader;
+    
+    public function __construct(
+        RepositoryInterface $usersRepository,
+        FactoryInterface $userInfoFactory,
+        FactoryInterface $avatarImageFactory,
+        FileUploaderInterface $imageUploader
+    ) {
+        $this->usersRepository      = $usersRepository;
+        $this->userInfoFactory      = $userInfoFactory;
+        $this->avatarImageFactory   = $avatarImageFactory;
+        $this->imageUploader        = $imageUploader;
     }
     
-    public function rolesEasyuiComboTreeWithSelectedSource(
-        $userId,
-        Request $request
-        ): JsonResponse {
+    public function displayUserInfo( $userId, Request $request ): Response
+    {
+        $user       = $this->usersRepository->find( $userId );
+        $userInfo   = $user->info() ?: $this->userInfoFactory->createNew();
+        
+        return $this->render( '@VSUsers/UsersCrud/Partial/form_user_info.html.twig', [
+            'form'      => $this->createForm( UserInfoForm::class, $userInfo )->createView(),
+            'userInfo'  => $userInfo,
+            'user'      => $user,
+        ]);
+    }
+    
+    public function handleUserInfo( $userId, Request $request ): Response
+    {
+        $userInfo   = $this->userInfoFactory->createNew();
+        $form       = $this->createForm( UserInfoForm::class, $userInfo );
+        $em         = $this->getDoctrine()->getManager();
+        
+        $form->handleRequest( $request );
+        if ( $form->isSubmitted() ) {
+            $userInfo   = $form->getData();
+            $user       = $this->usersRepository->find( $userId );
+
+            $profilePictureFile = $form->get( 'profilePicture' )->getData();
+            if ( $profilePictureFile ) {
+                $this->createAvatar( $userInfo, $profilePictureFile );
+            }
+            
+            $userInfo->setUser( $user );
+            $em->persist( $userInfo );
+            $em->flush();
+            
+            return $this->redirectToRoute( 'vs_users_profile_show' );
+        }
+    }
+    
+    public function rolesEasyuiComboTreeWithSelectedSource( $userId, Request $request ): JsonResponse
+    {
             $selectedRoles  = $userId ? $this->usersRepository->find( $userId )->getRoles() : [];
             $data           = [];
             $this->buildEasyuiCombotreeData( UserRole::choicesTree(), $data, $selectedRoles );
@@ -49,6 +104,20 @@ class UsersExtController extends AbstractController
                 
                 $key++;
             }
+        }
+    }
+    
+    protected function createAvatar( UserInfoInterface &$userInfo, File $file ): void
+    {
+        $avatarImage    = $userInfo->getAvatar() ?: $this->avatarImageFactory->createNew();
+        $uploadedFile   = new UploadedFile( $file->getRealPath(), $file->getBasename() );
+        
+        $avatarImage->setFile( $uploadedFile );
+        $this->imageUploader->upload( $avatarImage );
+        $avatarImage->setFile( null ); // reset File Because: Serialization of 'Symfony\Component\HttpFoundation\File\UploadedFile' is not allowed
+        
+        if ( ! $userInfo->getAvatar() ) {
+            $userInfo->setAvatar( $avatarImage );
         }
     }
 }
---
+++ src/Vankosoft/UsersBundle/Form/UserInfoForm.php
@@ -0,0 +1,60 @@
+<?php namespace VS\UsersBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use VS\UsersBundle\Model\UserInfo;
+
+class UserInfoForm extends AbstractType
+{
+    public function buildForm( FormBuilderInterface $builder, array $options ): void
+    {        
+        $builder
+            ->setMethod( 'POST' )
+            ->add( 'profilePicture', FileType::class, [
+                'label'                 => 'vs_users.form.profile.picture_lable',
+                'translation_domain'    => 'VSUsersBundle',
+                'mapped'                => false,
+                
+                // make it optional so you don't have to re-upload the Profile Image
+                // every time you edit the Profile details
+                'required'              => false,
+                
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints'           => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'vs_users.form.profile.picture_info',
+                    ])
+                ],
+            ])
+            
+            ->add( 'firstName', TextType::class, [
+                'label'                 => 'vs_users.form.user.firstName',
+                'translation_domain'    => 'VSUsersBundle',
+            ])
+            
+            ->add( 'lastName', TextType::class, [
+                'label'                 => 'vs_users.form.user.lastName',
+                'translation_domain'    => 'VSUsersBundle',
+            ])
+        ;
+    }
+    
+    public function configureOptions( OptionsResolver $resolver ): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserInfo::class,
+        ]);
+    }
+}
