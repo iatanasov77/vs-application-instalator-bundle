4ccc022e60228cc9fe272ca98b3cbcd775ab4597
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-12-07 10:38:30 +0000

    Fix Stupid Bug.

--- src/Vankosoft/UsersBundle/Form/UserFormType.php
+++ src/Vankosoft/UsersBundle/Form/UserFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 use VS\ApplicationBundle\Model\Application;
 use VS\UsersBundle\Model\UserInterface;
