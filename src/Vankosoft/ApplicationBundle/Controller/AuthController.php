1557b0138c67baec3b2810b997949478febbc69a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-12-20 16:53:14 +0000

    Imrove Application AuthController to not open login page when user is logged in.

--- src/Vankosoft/ApplicationBundle/Controller/AuthController.php
+++ src/Vankosoft/ApplicationBundle/Controller/AuthController.php
@@ -8,6 +8,10 @@ class AuthController extends AbstractController
 {
     public function login( AuthenticationUtils $authenticationUtils ): Response
     {
+        if ( $this->getUser() && $this->isGranted( 'ROLE_SUPER_ADMIN', $this->getUser() ) ) {
+            return $this->redirectToRoute( 'vs_application_dashboard' );
+        }
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         //var_dump($error); die;
