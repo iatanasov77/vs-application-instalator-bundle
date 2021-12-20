7601954423fb7cd3a17e70e046b11b4139c9c615
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-12-20 19:56:38 +0000

    Squashed 'src/Vankosoft/UsersBundle/' content from commit 7933e3f
    git-subtree-dir: src/Vankosoft/UsersBundle
    git-subtree-split: 7933e3f1bfae7e645a93a2ccea62245498461f1d

---
+++ Security/Voter/ApplicationVoter.php
@@ -0,0 +1,57 @@
+<?php namespace Vankosoft\UsersBundle\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+use Vankosoft\ApplicationBundle\Component\Context\ApplicationContextInterface;
+use Vankosoft\UsersBundle\Model\UserInterface;
+
+class ApplicationVoter implements VoterInterface
+{
+    private $security;
+    
+    private ApplicationContextInterface $applicationContext;
+    
+    public function __construct(
+        ApplicationContextInterface $applicationContext,
+        Security $security  = null
+    ) {
+            $this->applicationContext   = $applicationContext;
+            $this->security             = $security;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function vote( TokenInterface $token, $subject, array $attributes )
+    {
+        if ( ! $subject instanceof Request ) {
+            return self::ACCESS_ABSTAIN;
+        }
+        $user   = $token->getUser();
+        
+        if ( ! $user instanceof UserInterface ) {
+            return self::ACCESS_DENIED;
+        }
+        
+        if ( $user->hasRole( 'ROLE_SUPER_ADMIN' ) ) {
+            return self::ACCESS_GRANTED;
+        }
+        
+        if ( $user->getApplications()->isEmpty() ) {
+            return self::ACCESS_ABSTAIN;
+        }
+        
+        $application    = $this->applicationContext->getApplication();
+        $uri            = $subject->getUri(); // $uri may be needed sometimes
+        foreach ( $user->getApplications() as $userApplication ) {
+            if ( $userApplication == $application ) {
+                return self::ACCESS_GRANTED;
+            }
+        }
+        
+        return self::ACCESS_DENIED;
+    }
+}
