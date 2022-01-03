f88d4c501e42d019a10a31944bc14570fec9e250
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-01-03 21:45:18 +0000

    Optimize Application Kernel..

--- src/Vankosoft/ApplicationBundle/Component/Application/Kernel.php
+++ src/Vankosoft/ApplicationBundle/Component/Application/Kernel.php
@@ -46,18 +46,58 @@ abstract class Kernel extends HttpKernel
         return $this->getVarDir() . '/' . static::APP_ID . '/log';
     }
     
+    /**
+     * Override MicroKernelTrait::registerBundles()
+     *
+     * {@inheritdoc}
+     */
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->__getConfigDir() . '/bundles.php';
+        foreach ( $contents as $class => $envs ) {
+            if ( $envs[$this->environment] ?? $envs['all'] ?? false ) {
+                yield new $class();
+            }
+        }
+    }
+    
+    protected function configureContainer( ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder ): void
+    {
+        // Set Shared Sessions Path
+        $builder->setParameter( 'vs_application.session_save_path', $this->getVarDir() . '/sessions/' );
+        
+        $configDir  = $this->__getConfigDir();
+        
+        $container->import( $configDir . '/{packages}/*.yaml' );
+        $container->import( $configDir . '/{packages}/'. $this->environment . '/*.yaml' );
+        
+        if ( is_file( $configDir . '/services.yaml' ) ) {
+            $container->import( $configDir . '/services.yaml' );
+            $container->import( $configDir . '/{services}_' . $this->environment.'.yaml' );
+        } else {
+            $container->import( $configDir . '/{services}.php' );
+        }
+    }
+    
+    
+    protected function configureRoutes( RoutingConfigurator $routes ): void
+    {
+        $configDir = $this->__getConfigDir();
+        
+        $routes->import( $configDir . '/{routes}/' . $this->environment . '/*.yaml' );
+        $routes->import( $configDir . '/{routes}/*.yaml' );
+        
+        if ( is_file( $configDir . '/routes.yaml' ) ) {
+            $routes->import( $configDir . '/routes.yaml' );
+        } else {
+            $routes->import( $configDir . '/{routes}.php' );
+        }
+    }
+    
     protected function isVagrantEnvironment(): bool
     {
         return ( getenv( 'HOME' ) === '/home/vagrant' || getenv( 'VAGRANT' ) === 'VAGRANT' ) && is_dir( '/dev/shm' );
     }
     
-    /**
-     * {@inheritdoc}
-     */
-    abstract protected function configureContainer( ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder ): void;
-    
-    /**
-     * {@inheritdoc}
-     */
-    abstract protected function configureRoutes( RoutingConfigurator $routes ): void;
+    abstract protected function __getConfigDir(): string;
 }
