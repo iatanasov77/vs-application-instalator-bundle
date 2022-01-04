52909c85ff2e9b32ac54c63e187ad2f0fc44624e
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-01-04 17:47:26 +0000

    Squashed 'src/Vankosoft/CmsBundle/' content from commit 5d43a53
    git-subtree-dir: src/Vankosoft/CmsBundle
    git-subtree-split: 5d43a5301027b9d8ff9990f3222c49fbb649a545

---
+++ Controller/TocPageController.php
@@ -0,0 +1,9 @@
+<?php namespace Vankosoft\CmsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Vankosoft\ApplicationBundle\Controller\AbstractCrudController;
+
+class TocPageController extends AbstractCrudController
+{
+    
+}
