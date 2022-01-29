e4ccafc73c5c82e7de58534f9d11a353dd545cec
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-01-29 05:12:14 +0000

    Set Application Version After New Project is Installed.

--- src/Vankosoft/ApplicationInstalatorBundle/Installer/Setup/ApplicationSetup.php
+++ src/Vankosoft/ApplicationInstalatorBundle/Installer/Setup/ApplicationSetup.php
@@ -138,6 +138,8 @@ class ApplicationSetup
         if ( $this->newProjectInstall && $filesystem->exists( $projectRootDir . '/VERSION' ) ) {
             $this->applicationVersion   = file_get_contents( $projectRootDir . '/VERSION' );
             $filesystem->remove( $projectRootDir . '/VERSION' );
+        } else {
+            $this->applicationVersion   = App\AdminPanelKernel::VERSION;
         }
     }
     
