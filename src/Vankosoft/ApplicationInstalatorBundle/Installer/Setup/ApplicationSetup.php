a026f17d73fc5681ccc60929af7b201cc0d7cf24
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-01-29 10:57:08 +0000

    On Installer Move SetupApplicationTheme into CreateApplicationCommand.

--- src/Vankosoft/ApplicationInstalatorBundle/Installer/Setup/ApplicationSetup.php
+++ src/Vankosoft/ApplicationInstalatorBundle/Installer/Setup/ApplicationSetup.php
@@ -108,6 +108,28 @@ class ApplicationSetup
         $this->setupInstalationInfo();
     }
     
+    public function configureApplicationTheme( $themeTitle )
+    {
+        $themeSlug      = $this->slugGenerator->generate( $themeTitle );
+        $themeBuildKey  = $this->slugGenerator->generateCamelCase( $themeSlug );
+        
+        $projectRootDir = $this->container->get( 'kernel' )->getProjectDir();
+        $configFile     = $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/webpack_encore.yaml';
+        try {
+            $yamlArray  = Yaml::parseFile( $configFile );
+            
+            $yamlArray['framework']['assets']['packages'][$themeSlug]['json_manifest_path']
+                    = '%kernel.project_dir%/public/shared_assets/build/' . $themeSlug . '/manifest.json';
+            $yamlArray['webpack_encore']['builds'][$themeBuildKey]
+                    = '%kernel.project_dir%/public/shared_assets/build/' . $themeSlug;
+            
+            // https://stackoverflow.com/questions/58547953/symfony-yaml-formatting-the-output
+            \file_put_contents( $configFile, Yaml::dump( $yamlArray, 6 ) );
+        } catch ( ParseException $exception ) {
+            printf( 'Unable to parse the YAML string: %s', $exception->getMessage() );
+        }
+    }
+    
     public function setupAdminPanelKernel()
     {
         $this->newProjectInstall    = true;
