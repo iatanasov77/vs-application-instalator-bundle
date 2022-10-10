84253e352dc31592d4f2c127a57a423a09def30e
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-10 16:20:48 +0000

    Commit.

--- src/Vankosoft/UsersBundle/Form/Traits/UserInfoFormTrait.php
+++ src/Vankosoft/UsersBundle/Form/Traits/UserInfoFormTrait.php
@@ -11,40 +11,40 @@ trait UserInfoFormTrait
     public function buildUserInfoForm( FormBuilderInterface &$builder, array $options ): void
     {
         $builder
-        ->add( 'profilePicture', FileType::class, [
-            'label'                 => 'vs_users.form.profile.picture_lable',
-            'translation_domain'    => 'VSUsersBundle',
-            'mapped'                => $options['profilePictureMapped'],
+            ->add( 'profilePicture', FileType::class, [
+                'label'                 => 'vs_users.form.profile.picture_lable',
+                'translation_domain'    => 'VSUsersBundle',
+                'mapped'                => $options['profilePictureMapped'],
+                
+                // make it optional so you don't have to re-upload the Profile Image
+                // every time you edit the Profile details
+                'required'              => false,
+                
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints'           => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'vs_users.form.profile.picture_info',
+                    ])
+                ],
+            ])
             
-            // make it optional so you don't have to re-upload the Profile Image
-            // every time you edit the Profile details
-            'required'              => false,
+            ->add( 'firstName', TextType::class, [
+                'label'                 => 'vs_users.form.user.firstName',
+                'translation_domain'    => 'VSUsersBundle',
+                'mapped'                => $options['firstNameMapped'],
+            ])
             
-            // unmapped fields can't define their validation using annotations
-            // in the associated entity, so you can use the PHP constraint classes
-            'constraints'           => [
-                new File([
-                    'maxSize' => '1024k',
-                    'mimeTypes' => [
-                        'image/jpeg',
-                        'image/png',
-                    ],
-                    'mimeTypesMessage' => 'vs_users.form.profile.picture_info',
-                ])
-            ],
-        ])
-        
-        ->add( 'firstName', TextType::class, [
-            'label'                 => 'vs_users.form.user.firstName',
-            'translation_domain'    => 'VSUsersBundle',
-            'mapped'                => $options['firstNameMapped'],
-        ])
-        
-        ->add( 'lastName', TextType::class, [
-            'label'                 => 'vs_users.form.user.lastName',
-            'translation_domain'    => 'VSUsersBundle',
-            'mapped'                => $options['lastNameMapped'],
-        ])
+            ->add( 'lastName', TextType::class, [
+                'label'                 => 'vs_users.form.user.lastName',
+                'translation_domain'    => 'VSUsersBundle',
+                'mapped'                => $options['lastNameMapped'],
+            ])
         ;
     }
 }
