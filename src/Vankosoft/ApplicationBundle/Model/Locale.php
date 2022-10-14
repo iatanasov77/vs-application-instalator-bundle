1a1cd2211755ff30ee49062841b795bf4269aae7
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-14 09:41:38 +0000

    Extend Locale Model with addititional fields.

--- src/Vankosoft/ApplicationBundle/Model/Locale.php
+++ src/Vankosoft/ApplicationBundle/Model/Locale.php
@@ -1,8 +1,57 @@
 <?php namespace Vankosoft\ApplicationBundle\Model;
 
 use Sylius\Component\Locale\Model\Locale as BaseLocale;
+use Sylius\Component\Resource\Model\TranslatableTrait;
+use Sylius\Component\Resource\Model\TranslationInterface;
+use Vankosoft\ApplicationBundle\Model\Interfaces\LocaleInterface;
 
-class Locale extends BaseLocale
+class Locale extends BaseLocale implements LocaleInterface
 {
+    use TranslatableTrait;
     
+    /**
+     * @var string|null
+     */
+    protected $title;
+    
+    /** @var string */
+    protected $translatableLocale;
+    
+    /**
+     * There is getName() Method at \Sylius\Component\Locale\Model\Locale
+     * 
+     * {@inheritDoc}
+     * @see \Vankosoft\ApplicationBundle\Model\Interfaces\LocaleInterface::getTitle()
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+    
+    public function setTitle($title): LocaleInterface
+    {
+        $this->title = $title;
+        
+        return $this;
+    }
+    
+    public function getTranslatableLocale(): ?string
+    {
+        return $this->translatableLocale;
+    }
+    
+    public function setTranslatableLocale($translatableLocale): LocaleInterface
+    {
+        $this->translatableLocale   = $translatableLocale;
+        
+        return $this;
+    }
+    
+    /*
+     * @NOTE: Decalared abstract in TranslatableTrait
+     */
+    protected function createTranslation(): TranslationInterface
+    {
+        
+    }
 }
