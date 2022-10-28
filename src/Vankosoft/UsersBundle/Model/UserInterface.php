7691f270c85a7d1a426dcfd4797b1b3c49cb96c0
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-28 19:48:03 +0000

    Fix UserInterface .

--- src/Vankosoft/UsersBundle/Model/UserInterface.php
+++ src/Vankosoft/UsersBundle/Model/UserInterface.php
@@ -1,10 +1,12 @@
 <?php namespace Vankosoft\UsersBundle\Model;
 
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 use Sylius\Component\Resource\Model\ResourceInterface;
 use Doctrine\Common\Collections\Collection;
 
-interface UserInterface extends BaseUserInterface, ResourceInterface
+interface UserInterface extends BaseUserInterface, PasswordAuthenticatedUserInterface, ResourceInterface
 {
     public function hasRole( string $role ): bool;
     
