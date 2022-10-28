5ad228cd2e0c17f9274bce7b62c17d05b9b79235
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-28 17:05:13 +0000

    Fix Model User .

--- src/Vankosoft/UsersBundle/Model/User.php
+++ src/Vankosoft/UsersBundle/Model/User.php
@@ -91,7 +91,7 @@ class User implements UserInterface
      *
      * @return array
      */
-    public function getRoles()
+    public function getRoles(): array
     {
         return $this->getRolesFromArray();
         
