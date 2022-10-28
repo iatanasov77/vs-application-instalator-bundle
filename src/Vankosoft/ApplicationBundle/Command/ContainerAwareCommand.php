40fce90206eaefa729684857126f59afff6c6f67
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-28 18:34:51 +0000

    Fix Class Vankosoft\ApplicationBundle\Command\ContainerAwareCommand .

--- src/Vankosoft/ApplicationBundle/Command/ContainerAwareCommand.php
+++ src/Vankosoft/ApplicationBundle/Command/ContainerAwareCommand.php
@@ -2,7 +2,8 @@
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+//use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\Container\ContainerInterface
 
 /**
  * @link https://github.com/symfony/symfony/blob/v4.4.18/src/Symfony/Bundle/FrameworkBundle/Command/ContainerAwareCommand.php
