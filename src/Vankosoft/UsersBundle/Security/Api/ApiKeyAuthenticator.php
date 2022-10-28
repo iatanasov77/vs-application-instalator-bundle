bd1d8e12ce0906881069af599a9d9cae8038a0cd
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-10-28 16:59:42 +0000

    Fix Class ApiKeyAuthenticator .

--- src/Vankosoft/UsersBundle/Security/Api/ApiKeyAuthenticator.php
+++ src/Vankosoft/UsersBundle/Security/Api/ApiKeyAuthenticator.php
@@ -10,7 +10,6 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -42,7 +41,7 @@ class ApiKeyAuthenticator extends AbstractAuthenticator
         return $request->headers->has( 'X-AUTH-TOKEN' );
     }
     
-    public function authenticate( Request $request ): PassportInterface
+    public function authenticate( Request $request ): Passport
     {
         $email      = 'email@example.com';
         //$apiToken = $request->cookies->get( 'api_token' ),
