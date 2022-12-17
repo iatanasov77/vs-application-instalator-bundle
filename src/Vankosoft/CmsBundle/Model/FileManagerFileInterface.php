f478b5c35a04e0f2a42b76c684d6bd7b37d9b04f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-12-17 07:58:22 +0000

    Remove Component: CmsBundle

--- src/Vankosoft/CmsBundle/Model/FileManagerFileInterface.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Vankosoft\CmsBundle\Model;
-
-interface FileManagerFileInterface extends FileInterface
-{
-    public function getFilemanager(): FileManagerInterface;
-    public function setFilemanager( FileManagerInterface $filemanager ): self;
-}
