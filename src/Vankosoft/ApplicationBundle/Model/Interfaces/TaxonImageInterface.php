23ce3bdbe26d3b0d2b0f85ebca9d0777d5ae7cd1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-12-17 07:53:32 +0000

    Remove Component: ApplicationBundle

--- src/Vankosoft/ApplicationBundle/Model/Interfaces/TaxonImageInterface.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Vankosoft\ApplicationBundle\Model\Interfaces;
-
-use Vankosoft\CmsBundle\Model\FileInterface;
-
-interface TaxonImageInterface extends FileInterface
-{
-}
--- src/Vankosoft/ApplicationBundle/Model/Interfaces/TaxonRelationInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Vankosoft\ApplicationBundle\Model\Interfaces;
-
-use Sylius\Component\Resource\Model\ResourceInterface;
-use Sylius\Component\Resource\Model\TranslatableInterface;
-
-interface TaxonRelationInterface extends ResourceInterface, TranslatableInterface
-{
-    
-}
