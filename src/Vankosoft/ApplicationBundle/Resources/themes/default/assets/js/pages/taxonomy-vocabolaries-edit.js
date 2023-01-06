a829594a63d9e1467d621c9c10c076c290211568
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-01-06 07:03:50 +0000

    Fully Remove Using Bootstrap GTreeTable.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/taxonomy-vocabolaries-edit.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/taxonomy-vocabolaries-edit.js
@@ -1,27 +1,12 @@
-//require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.css' );
-//require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.js' );
 require( '@kanety/jquery-simple-tree-table/dist/jquery-simple-tree-table.js' );
 require( 'jquery-easyui/css/easyui.css' );
 require( 'jquery-easyui/js/jquery.easyui.min.js' );
 
+/**
+ * Sortable Is Not Implemented. If Needed For Example See 'toc-pages.js'
+ */
 $( function()
 {
-/*
-	$( '#tableTaxons' ).gtreetable({
-		'source': function ( id ) {
-			return {
-				type: 'GET',
-				url: $( '#tableTaxons' ).attr( 'data-url' ),
-				data: { 'parentTaxonId': id },
-				dataType: 'json',
-				error: function( XMLHttpRequest ) {
-					alert( 'GTreeTable ERROR !!!' );
-					alert( XMLHttpRequest.status + ': ' + XMLHttpRequest.responseText );
-				}
-  	      	}
-  	    }
-  	});
-*/
   	$( '#tableTaxons' ).simpleTreeTable({
         expander: $( '#expander' ),
         collapser: $( '#collapser' ),
