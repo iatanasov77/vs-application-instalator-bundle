8c4c20dc415f870fa141e28caf386dfbf8aacf23
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-01-06 05:07:36 +0000

    Change GTreeTable With SimpleTreeTable on Taxonomy Edit Page.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/taxonomy-vocabolaries-edit.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/taxonomy-vocabolaries-edit.js
@@ -1,10 +1,12 @@
-require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.css' );
-require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.js' );
+//require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.css' );
+//require( 'bootstrap-gtreetable/dist/bootstrap-gtreetable.js' );
+require( '@kanety/jquery-simple-tree-table/dist/jquery-simple-tree-table.js' );
 require( 'jquery-easyui/css/easyui.css' );
 require( 'jquery-easyui/js/jquery.easyui.min.js' );
 
 $( function()
 {
+/*
 	$( '#tableTaxons' ).gtreetable({
 		'source': function ( id ) {
 			return {
@@ -19,4 +21,15 @@ $( function()
   	      	}
   	    }
   	});
+*/
+  	$( '#tableTaxons' ).simpleTreeTable({
+        expander: $( '#expander' ),
+        collapser: $( '#collapser' ),
+        opened: []
+    });
+    
+    $( '#collapsed' ).simpleTreeTable({
+        //opened: 'all',
+        opened: []
+    });
 });
