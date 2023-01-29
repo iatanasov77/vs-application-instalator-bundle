a1d219b05abea90559ffac969c5a06fba4f35716
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-01-29 21:24:36 +0000

    Add jQueryFileUpload.js

---
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/jQueryFileUpload.js
@@ -0,0 +1,115 @@
+require( 'jquery-ui-dist/jquery-ui.js' );
+require( 'jquery-ui-dist/jquery-ui.css' );
+require( 'jquery-ui-dist/jquery-ui.theme.css' );
+require( 'blueimp-file-upload/js/jquery.fileupload.js' );
+
+// WORKAROUND: Prevent Double Submiting
+global.btnSaveUploadFileClicked = window.btnSaveUploadFileClicked = false;
+
+function  callProgressBar()
+{
+    $( "#progressbar" ).progressbar({
+        value: 0
+    });
+ }; 
+     
+export function InitOneUpFileUpload()
+{
+    ///////////////////////////////////////////////////////////////////////
+    // https://github.com/blueimp/jQuery-File-Upload/wiki/Options
+    ///////////////////////////////////////////////////////////////////////
+    $( '#OneUpFileUpload' ).fileupload({
+        url: '' + $( '#OneUpFileUpload' ).attr( 'data-endpoint' ),
+        type: 'POST',
+        dropZone: null,
+        fileInput: $( '#upload_file_form_file' ),
+        maxChunkSize: 1000000,
+        autoUpload: false,
+        add: function ( e, data )
+        {
+            $( '#btnSaveUploadFile' ).on( 'click', function ( e )
+            {
+                e.preventDefault();
+                e.stopPropagation();
+                
+                if ( window.btnSaveUploadFileClicked ) {
+                    return;
+                }
+                window.btnSaveUploadFileClicked   = true;
+                
+                $( this ).hide();
+                data.submit();
+            });
+        },
+        formData: function ( form )
+        {
+            //return form.serializeArray();
+            
+            /*
+             * Send Values Needed For PostPersistListener In Backend
+             *
+             * If Files is Not Wrapped by Form Name Remove It From Here
+             */
+            return [
+                {
+                    name: 'formName',
+                    value: $( '#formUpload' ).attr( 'name' )
+                },
+                {
+                    name: 'fileResourceId',
+                    value: $( '#FileResourceId' ).val()
+                },
+                {
+                    name: 'fileResourceClass',
+                    value: $( '#FileResourceClass' ).val()
+                },
+                {
+                    name: 'fileResourceOwner',
+                    value: $( '#FileOwnerId' ).val()
+                },
+                {
+                    name: 'fileOwnerClass',
+                    value: $( '#FileOwnerClass' ).val()
+                }
+            ];
+        }
+    });
+    
+    /**
+     * FileUpload Event Listeners
+     * ===============
+     * https://github.com/blueimp/jQuery-File-Upload/wiki/Options#callback-options
+     */
+    $( '#FileUploadProgressbar' ).progressbar({
+        value: 0
+    });
+    
+    $( '#OneUpFileUpload' ).on( 'fileuploadstart', function ( e, data )
+    {
+        $( '#FileUploadProgressbar' ).show();
+    });
+    
+    $( '#OneUpFileUpload' ).on( 'fileuploadprogress', function ( e, data )
+    {
+        //console.log( data.loaded, data.total, data.bitrate );
+        $( '#FileUploadProgressbar' ).progressbar({
+            value: data.loaded,
+            max: data.total
+        });
+        $( '#FileUploadProgressbar' ).children( 'span.caption' ).html( data.loaded + ' / ' + data.total );
+        $( '#FileUploadProgressbar' ).children( '.ui-progressbar-value' ).css( "display", "block" );
+    });
+    
+    // Uncomment Console Logs For Debugging
+    $( '#OneUpFileUpload' ).on( 'fileuploaddone', function ( e, data )
+    {
+        e.preventDefault();
+        e.stopPropagation();
+        $( '#FileUploadProgressbar' ).hide();
+        
+        //console.log( 'FileUploadDone: ' );
+        //console.log( data.result );
+        
+        document.location   = document.location;
+    });
+}
