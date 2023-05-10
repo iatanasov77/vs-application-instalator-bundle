e8a61acffcd38ecf52483bc2b30986e2824fac5b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-10 13:45:44 +0000

    Create an ExceptionListener.

--- src/Vankosoft/ApplicationBundle/EventListener/ExceptionListener.php
+++ src/Vankosoft/ApplicationBundle/EventListener/ExceptionListener.php
@@ -4,29 +4,54 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
+use Vankosoft\ApplicationBundle\Component\Exception\TestException;
+use Vankosoft\ApplicationBundle\Component\Context\ApplicationNotFoundException;
+
 class ExceptionListener
 {
     public function onKernelException( ExceptionEvent $event )
     {
-        $exception = $event->getThrowable();
-        $message = sprintf(
-            'Getting following error: %s with code: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
+        $exception  = $event->getThrowable();
+        $response   = new Response();
         
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent( $message );
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ( $exception instanceof HttpExceptionInterface ) {
-            $response->setStatusCode( $exception->getStatusCode() );
-            $response->headers->replace( $exception->getHeaders() );
-        } else {
-            $response->setStatusCode( Response::HTTP_INTERNAL_SERVER_ERROR );
+        switch ( true ) {
+            case  ( $exception instanceof TestException ):
+                $message = sprintf(
+                    'Getting following error: %s with code: %s',
+                    $exception->getMessage(),
+                    $exception->getCode()
+                );
+                
+                // Customize your response object to display the exception details
+                $response->setContent( $message );
+                
+                // sends the modified response object to the event
+                $event->setResponse( $response );
+                
+                break;
+            case ( $exception instanceof ApplicationNotFoundException ):
+                $message = sprintf(
+                    'VankoSoft Application Error: %s with code: %s',
+                    $exception->getMessage(),
+                    $exception->getCode()
+                );
+                
+                // Customize your response object to display the exception details
+                $response->setContent( $message );
+                
+                // sends the modified response object to the event
+                $event->setResponse( $response );
+                
+                break;
+                
+            case ( $exception instanceof HttpExceptionInterface ):
+                $response->setStatusCode( $exception->getStatusCode() );
+                $response->headers->replace( $exception->getHeaders() );
+                
+                // sends the modified response object to the event
+                $event->setResponse( $response );
+                
+                break;
         }
-        // sends the modified response object to the event
-        $event->setResponse( $response );
     }
 }
