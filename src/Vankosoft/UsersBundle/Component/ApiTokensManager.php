341060c8cb5668aa8d80677a4ec1f572fed3aa46
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-18 14:18:43 +0000

    Create an ApiTokensManager Service.

---
+++ src/Vankosoft/UsersBundle/Component/ApiTokensManager.php
@@ -0,0 +1,30 @@
+<?php namespace Vankosoft\UsersBundle\Component;
+
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Model\VerifyEmailSignatureComponents;
+use Vankosoft\UsersBundle\Model\UserInterface;
+
+class ApiTokensManager
+{
+    /**
+     * @var VerifyEmailHelperInterface
+     */
+    private $verifyEmailHelper;
+    
+    public function __construct( VerifyEmailHelperInterface $helper )
+    {
+        $this->verifyEmailHelper    = $helper;
+    }
+    
+    public function getVerifySignature( UserInterface $oUser, string $signatureRoute ): VerifyEmailSignatureComponents
+    {
+        $signature  = $this->verifyEmailHelper->generateSignature(
+            $signatureRoute,
+            $oUser->getId(),
+            $oUser->getEmail(),
+            ['id' => $oUser->getId()]
+        );
+        
+        return $signature;
+    }
+}
