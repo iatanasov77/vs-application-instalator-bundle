6dca0962c695cb7d3166b3628923aa4e949adaef
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-20 02:42:21 +0000

    Add User Profile Page Tab Anchors.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/profile.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/profile.js
@@ -36,4 +36,18 @@ $( function()
             }
         });
     });
+    
+    var hash = location.hash.replace( /^#/, '' );
+    if ( hash ) {
+        var someVarName = $( '.nav-tabs a[href="#' + hash + '"]' );
+        var tab         = new bootstrap.Tab( someVarName );
+        tab.show();
+    }
+    
+    $( '.nav-tabs a' ).on( 'shown.bs.tab', function ( e )
+    {
+        window.location.hash = e.target.hash;
+        window.scrollTo( 0, 0 );
+    });
+    
 });
