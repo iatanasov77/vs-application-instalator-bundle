1cf12bc4dc9fbf8a3ee77b5824a6bd83d38e2ed4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-04 11:15:18 +0000

    Add Routes and Templates for CookieConsentTranslation Resource.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/webpack.config.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/webpack.config.js
@@ -61,6 +61,8 @@ Encore
     .addEntry( 'js/profile', assetsPath + '/js/pages/profile.js' )
     .addEntry( 'js/taxonomy-vocabolaries', assetsPath + '/js/pages/taxonomy-vocabolaries.js' )
     .addEntry( 'js/taxonomy-vocabolaries-edit', assetsPath + '/js/pages/taxonomy-vocabolaries-edit.js' )
+    .addEntry( 'js/locales', assetsPath + '/js/pages/locales.js' )
+    .addEntry( 'js/cookie-consent-translations', assetsPath + '/js/pages/cookie-consent-translations.js' )
     
     .addEntry( 'js/pages-categories', assetsPath + '/js/pages/pages_categories.js' )
     .addEntry( 'js/pages-categories-edit', assetsPath + '/js/pages/pages_categories_edit.js' )
