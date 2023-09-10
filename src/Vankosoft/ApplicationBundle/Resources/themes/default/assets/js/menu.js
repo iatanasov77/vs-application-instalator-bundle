b3f1be2fb53d0e4010ebc6eb432298681ad029c3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-10 11:28:58 +0000

    Fix Active Main Menu Elements.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/menu.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/menu.js
@@ -4,6 +4,13 @@ $( function()
     $( '.main-menu-current-item' ).closest( '.submenu' ).addClass( 'show' );
     
     var activePosition  = $( '.main-menu-current-item' ).position();
+    if( ! activePosition ) {
+        var activeItemPath  = $( 'ol.breadcrumb' ).children( 'li.breadcrumb-item' ).find( 'a' ).attr( 'href' );
+        activePosition      = $( 'a.nav-link[href="' + activeItemPath + '"]' ).position();
+        
+        alert( activeItemPath );
+    }
+    
     if ( activePosition ) {
         $( '.menu-list' ).slimScroll({
             scrollTo: ( activePosition.top - 50 ) + 'px',
