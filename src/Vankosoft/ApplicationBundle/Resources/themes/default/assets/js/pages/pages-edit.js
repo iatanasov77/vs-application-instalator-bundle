cc73ac685553e9a477d0e59cb92c128ac8781838
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-14 13:00:13 +0000

    Improve CMS Page Update Form.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/pages-edit.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/pages-edit.js
@@ -3,18 +3,20 @@ require( 'jquery-easyui/js/jquery.easyui.min.js' );
 // Need copy of: jquery-easyui/images/*
 
 require( '../includes/clone_preview.js' );
+import { VsPath } from '../includes/fos_js_routes.js';
+
 $( function()
 {
 	$( '#page_form_locale' ).on( 'change', function( e ) {
-		var pageId	= $( '#pageFormContainer' ).attr( 'data-pageId' );
+		var pageId	= $( '#FormContainer' ).attr( 'data-itemId' );
 		var locale	= $( this ).val()
 		
 		if ( pageId ) {
     		$.ajax({
                 type: 'GET',
-                url: '/page-actions/get-form/' + locale + '/' + pageId,
+                url: VsPath( 'vs_cms_pages_form_in_locale', { 'itemId': pageId, 'locale': locale } ),
                 success: function ( data ) {
-                    $( '#pageFormContainer' ).html( data );
+                    $( '#FormContainer' ).html( data );
                     $( '#page_form_category_taxon' ).combotree();
                 }, 
                 error: function(XMLHttpRequest, textStatus, errorThrown) {
