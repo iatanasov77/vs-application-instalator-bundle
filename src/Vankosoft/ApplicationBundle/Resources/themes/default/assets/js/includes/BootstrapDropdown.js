8c1e64275a4866f3e87e5969e866ce815a51e7e4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-15 15:00:01 +0000

    Fix AdminPanel Notifications DropDown on Custom Entries.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/BootstrapDropdown.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/BootstrapDropdown.js
@@ -2,18 +2,19 @@
  *  Fix Bootstrap Dropdowns on Custom Entries
  */
 
-var bootstrap               = require( 'bootstrap' );
-window.btnProfileClicked    = false;
+var bootstrap                   = require( 'bootstrap' );
+window.btnProfileClicked        = false;
+window.btnNotificationsClicked  = false;
 
 $( function()
 {
-    let toggleEl   = $( '#navbarDropdownMenuLink2' );
-    toggleEl.on( 'click', function( e )
+    let toggleProfileEl   = $( '#navbarDropdownMenuLink2' );
+    toggleProfileEl.on( 'click', function( e )
     {
-        let toggleBtn   = toggleEl[0];
+        let toggleBtn   = toggleProfileEl[0];
         let dropdownEl  = new bootstrap.Dropdown( toggleBtn );
         
-        let profileMenu = toggleEl.next( '.dropdown-menu' );
+        let profileMenu = toggleProfileEl.next( '.dropdown-menu' );
         if ( profileMenu.hasClass( 'show' ) ) {
             if ( window.btnProfileClicked ) {
                 window.btnProfileClicked   = false;
@@ -26,6 +27,28 @@ $( function()
             //alert( 'NOT Displayed' );
             dropdownEl.show();
             window.btnProfileClicked   = true;
+        }
+    });
+    
+    let toggleNotificationsEl   = $( '#navbarDropdownMenuLink1' );
+    toggleNotificationsEl.on( 'click', function( e )
+    {
+        let toggleBtn   = toggleNotificationsEl[0];
+        let dropdownEl  = new bootstrap.Dropdown( toggleBtn );
+        
+        let notificationsMenu = toggleNotificationsEl.next( '.dropdown-menu' );
+        if ( notificationsMenu.hasClass( 'show' ) ) {
+            if ( window.btnNotificationsClicked ) {
+                window.btnNotificationsClicked  = false;
+                return false;  
+            }
+            
+            //alert( 'Displayed' );
+            dropdownEl.toggle();
+        } else {
+            //alert( 'NOT Displayed' );
+            dropdownEl.show();
+            window.btnNotificationsClicked   = true;
         }
     });
 });
