7ef9b037d2ac501c13eb69ae2b2f033e17935e08
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-20 19:59:18 +0000

    Create Tags Whitelist Resources.

--- src/Vankosoft/ApplicationBundle/Model/TagsWhitelistContext.php
+++ src/Vankosoft/ApplicationBundle/Model/TagsWhitelistContext.php
@@ -3,6 +3,7 @@
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Vankosoft\ApplicationBundle\Model\Interfaces\TagsWhitelistContextInterface;
+use Vankosoft\ApplicationBundle\Model\Interfaces\TaxonInterface;
 
 class TagsWhitelistContext implements TagsWhitelistContextInterface
 {
