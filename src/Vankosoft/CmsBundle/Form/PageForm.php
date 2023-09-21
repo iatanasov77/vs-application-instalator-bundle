408c72fa56d237b2a3dcb9a9ab76bf02e9dbbe10
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-21 07:38:47 +0000

    Use Tags in CMS Page Model.

--- src/Vankosoft/CmsBundle/Form/PageForm.php
+++ src/Vankosoft/CmsBundle/Form/PageForm.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
