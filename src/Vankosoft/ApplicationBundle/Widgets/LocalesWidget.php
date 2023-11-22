9a9fb12b71358d3ef042eb3f1922cb42732ce185
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-11-22 06:50:04 +0000

    Create First Widget.

--- src/Vankosoft/ApplicationBundle/Widgets/LocalesWidget.php
+++ src/Vankosoft/ApplicationBundle/Widgets/LocalesWidget.php
@@ -10,22 +10,23 @@ class LocalesWidget
         // Get Widget Container
         $widgets = $event->getWidgetContainer();
         
+        // Create Widget Item
+        $widgetItem = ( new Item( 'user_info', 3600 ) ) // Add Cache Time or Default 3600 Second
+                        ->setGroup( 'admin' )
+                        ->setName( 'widget_user_info.name' )
+                        ->setDescription( 'widget_user_info.description' )
+                        ->setActive( true )
+                        
+                        ->setTemplate( '@VSApplication/Widgets/locales.html.twig' )
+                        //->setContent( 'pdWidget Text Content' )
+                        //->setRole( ['USER_INFO_WIDGET'] )
+                        
+                        ->setData( function () {
+                            return ['userCount' => 5];
+                        })
+                        ->setOrder( 5 );
+                        
         // Add Widgets
-        $widgets->addWidget(
-            ( new Item( 'user_info', 3600 ) ) // Add Cache Time or Default 3600 Second
-                ->setGroup( 'admin' )
-                ->setName( 'widget_user_info.name' )
-                ->setDescription( 'widget_user_info.description' )
-                ->setActive( true )
-            
-                ->setTemplate( '@VSApplication/Widgets/locales.html.twig' )
-                //->setContent( 'pdWidget Text Content' )
-                //->setRole( ['USER_INFO_WIDGET'] )
-                
-                ->setData( function () {
-                    return ['userCount' => 5];
-                })
-                ->setOrder( 5 )
-        );
+        $widgets->addWidget( $widgetItem );
     }
 }
