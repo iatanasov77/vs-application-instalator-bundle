d7d2a94e2ed3859998b47a1114055a7daeb9cc91
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-06 08:20:49 +0000

    Add an Action into Recieved Payments Page Java Script.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/payment_pages/recieved-payments.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/payment_pages/recieved-payments.js
@@ -1,7 +1,37 @@
 require( '../includes/resource-delete.js' );
 require( '../../vendor/vs_tablesortable/tablesortable.js' );
+import { VsPath } from '@/js/includes/fos_js_routes.js';
 
 $( function()
 {
-    
+    $( '#FormFilterPayments' ).on( 'submit', function( e )
+    {
+        e.preventDefault();
+        
+        let submitUrl   = VsPath( 'vs_payment_custom_payment_actions_search_payments' );
+        let formData    = new FormData( $( '#FormFilterPayments' )[ 0 ] );
+        
+        $.ajax({
+            type: "POST",
+            url: submitUrl,
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function( response )
+            {
+                if ( response.status == 'ok' ) {
+                    $( '#PaymentsTableContainer' ).html( response.data );
+                } else {
+                    alert( 'ERROR' );
+                }
+            },
+            error: function()
+            {
+                alert( "SYSTEM ERROR!!!" );
+            }
+        });
+        
+        return false;
+    });
 });
