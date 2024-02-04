5240a6b77cca477b5e10616ddf94a678f0957fbb
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-02-04 07:49:57 +0000

    Use Alerts Macros for Form Errors.

--- src/Vankosoft/ApplicationBundle/Resources/maker/resource/templates/default/_form.tpl.php
+++ src/Vankosoft/ApplicationBundle/Resources/maker/resource/templates/default/_form.tpl.php
@@ -1,16 +1,14 @@
 {% import "@VSApplication/Macros/form.html.twig" as vs_form %}
+{% import "@VSApplication/Macros/alerts.html.twig" as alerts %}
 
 {% if useFormMethod is defined %}{% set formMethod = useFormMethod %}{% else %}{% set formMethod = 'PUT' %}{% endif %}
 {{ form_start( form, { 'attr': {'class': 'form-horizontal', 'id': 'form-post'}, 'method': formMethod } ) }}
     {{ form_widget( form._token ) }}
     
-    {% if form_errors(form) is not empty %}
-        <div class="alert alert-block alert-danger fade in">
-            <button type="button" class="close close-sm" data-bs-dismiss="alert">
-                <i class="icon-remove"></i>
-            </button>
-            <strong>{{ 'vs_application.form.errors_title' | trans( {},'VSApplicationBundle' ) }}</strong> {{ form_errors( form ) }}
-        </div>
+    {% if not form.vars.valid %}
+        {% for error in form.vars.errors %}
+            {{ alerts.error( error.message ) }}
+        {%endfor%}
     {% endif %}
     
     {{ form_widget( form ) }}
