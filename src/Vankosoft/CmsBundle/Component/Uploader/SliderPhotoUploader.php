61f195de31d6943c0149771fb98133a4ee41c6e5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-02-27 04:31:22 +0000

    Register Gaufrete Filesystem for Slider Items Photos.

--- src/Vankosoft/CmsBundle/Component/Uploader/SliderPhotoUploader.php
+++ src/Vankosoft/CmsBundle/Component/Uploader/SliderPhotoUploader.php
@@ -4,8 +4,8 @@ use Gaufrette\Filesystem;
 use Symfony\Component\HttpFoundation\File\File;
 use Webmozart\Assert\Assert;
 
-use Vankosoft\CmsBundle\Component\Uploader\FileUploaderInterface;
 use Vankosoft\CmsBundle\Component\Generator\FilePathGeneratorInterface;
+use Vankosoft\CmsBundle\Component\Generator\UploadedFilePathGenerator;
 use Vankosoft\CmsBundle\Model\Interfaces\FileInterface;
 
 class SliderPhotoUploader implements FileUploaderInterface
@@ -18,18 +18,18 @@ class SliderPhotoUploader implements FileUploaderInterface
     
     public function __construct(
         Filesystem $filesystem,
-        FilePathGeneratorInterface $filePathGenerator
+        ?FilePathGeneratorInterface $filePathGenerator = null
     ) {
-            $this->filesystem = $filesystem;
-            
-            if ( $filePathGenerator === null ) {
-                @trigger_error( sprintf(
-                    'Not passing an $filePathGenerator to %s constructor is deprecated since Sylius 1.6 and will be not possible in Sylius 2.0.',
-                    self::class
-                ), \E_USER_DEPRECATED );
-            }
-            
-            $this->filePathGenerator = $filePathGenerator ?? new FilePathGenerator();
+        $this->filesystem = $filesystem;
+        
+        if ( $filePathGenerator === null ) {
+            @trigger_error( sprintf(
+                'Not passing an $filePathGenerator to %s constructor is deprecated since Sylius 1.6 and will be not possible in Sylius 2.0.',
+                self::class
+            ), \E_USER_DEPRECATED );
+        }
+        
+        $this->filePathGenerator = $filePathGenerator ?? new UploadedFilePathGenerator();
     }
     
     public function upload( FileInterface $sliderPhoto ): void
