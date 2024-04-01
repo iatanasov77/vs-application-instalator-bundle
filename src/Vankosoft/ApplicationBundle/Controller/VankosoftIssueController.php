8d62f4594903d6a94d8e1a5381705a469f319426
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-04-01 04:11:23 +0000

    add ProjectIssuesController.

---
+++ src/Vankosoft/ApplicationBundle/Controller/VankosoftIssueController.php
@@ -0,0 +1,51 @@
+<?php namespace Vankosoft\ApplicationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException;
+use Vankosoft\ApiBundle\Exception\ApiLoginException;
+
+class VankosoftIssueController extends AbstractController
+{
+    /** @var HttpClientInterface */
+    private $httpClient;
+    
+    /** @var CacheItemPoolInterface */
+    private $cache;
+    
+    public function __construct(
+        HttpClientInterface $httpClient,
+        CacheItemPoolInterface $cache
+    ) {
+        $this->httpClient   = $httpClient;
+        $this->cache        = $cache;
+    }
+    
+    public function indexAction( Request $request ): Response
+    {
+        $apiLoginUrl    = 'http://vankosoft.lh/api/login_check';
+        //$apiLoginUrl    = 'http://vankosoft.lh/api/project-issues';
+        
+        try {
+            $response       = $this->httpClient->request( 'POST', $apiLoginUrl, [
+                'json' => [
+                    'username' => 'admin',
+                    'password' => 'admin'
+                ],
+            ]);
+        }  catch ( JWTEncodeFailureException $e ) {
+            //throw new ApiLoginException( 'JWTEncodeFailureException: ' . $e->getMessage() );
+        }
+        
+        //echo '<pre>'; var_dump( $response ); die;
+        $payload = $response->toArray( false );
+        echo '<pre>'; var_dump( $payload ); die;
+        
+        return $this->render( '@VSApplication/Pages/ProjectIssues/index.html.twig', [
+            'issues'    => [],
+        ]);
+    }
+}
