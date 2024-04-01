dfd505683cece7c34c8c2c426782881b08e5b2b4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-04-01 10:02:25 +0000

    Add Project Option into VankoSoft API Configuration.

--- src/Vankosoft/ApplicationBundle/DependencyInjection/VSApplicationExtension.php
+++ src/Vankosoft/ApplicationBundle/DependencyInjection/VSApplicationExtension.php
@@ -42,9 +42,10 @@ class VSApplicationExtension extends AbstractResourceExtension implements Prepen
         
         // VankoSoft API
         $container->setParameter( 'vs_application.vankosoft_api.enabled', $config[ 'vankosoft_api' ]['enabled'] );
-        $container->setParameter( 'vs_application.vankosoft_api.host', $config[ 'vankosoft_api' ]['host'] );
-        $container->setParameter( 'vs_application.vankosoft_api.user', $config[ 'vankosoft_api' ]['user'] );
-        $container->setParameter( 'vs_application.vankosoft_api.password', $config[ 'vankosoft_api' ]['password'] );
+        $container->setParameter( 'vs_application.vankosoft_api.project', $config[ 'vankosoft_api' ]['project'] );
+        $container->setParameter( 'vs_application.vankosoft_api.host', $config[ 'vankosoft_api' ]['connection']['host'] );
+        $container->setParameter( 'vs_application.vankosoft_api.user', $config[ 'vankosoft_api' ]['connection']['user'] );
+        $container->setParameter( 'vs_application.vankosoft_api.password', $config[ 'vankosoft_api' ]['connection']['password'] );
     }
     
     public function prepend( ContainerBuilder $container ): void
