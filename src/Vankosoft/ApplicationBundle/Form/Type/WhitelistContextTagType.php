bf66c8c973a44bf49a1f3c0b44f352c27d7b5ff5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-04-04 10:57:26 +0000

    Creating Tags on Whitelist Context in AdminPanel.

--- src/Vankosoft/ApplicationBundle/Form/Type/WhitelistContextTagType.php
+++ src/Vankosoft/ApplicationBundle/Form/Type/WhitelistContextTagType.php
@@ -13,7 +13,7 @@ class WhitelistContextTagType extends AbstractType
     public function buildForm( FormBuilderInterface $builder, array $options )
     {
         $builder
-            ->add( 'id', HiddenType::class, ['mapped' => false] )
+            ->add( 'id', HiddenType::class )
             ->add( 'tag', TextType::class, [
                 'required'              => false,
                 'translation_domain'    => 'VSApplicationBundle',
