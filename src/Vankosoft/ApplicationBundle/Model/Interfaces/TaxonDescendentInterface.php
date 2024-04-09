5729d790eb8f5a6e5976c2c3b2827fbd5524a6e2
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-04-09 12:42:38 +0000

    Improve TaxonDescendentInterface for Models.

--- src/Vankosoft/ApplicationBundle/Model/Interfaces/TaxonDescendentInterface.php
+++ src/Vankosoft/ApplicationBundle/Model/Interfaces/TaxonDescendentInterface.php
@@ -3,6 +3,7 @@
 interface TaxonDescendentInterface
 {
     public function getTaxon(): ?TaxonInterface;
+    public function isEnabled(): bool;
     public function getSlug(): string;
     public function getCode(): string;
     public function getName(): string;
