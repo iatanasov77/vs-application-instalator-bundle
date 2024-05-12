2e8662be3f0ff2ac6daa4d809a5489562ebe11b0
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-05-12 08:19:57 +0000

    Improve CMS Pages Index View.

--- src/Vankosoft/ApplicationBundle/Controller/Traits/FilterFormTrait.php
+++ src/Vankosoft/ApplicationBundle/Controller/Traits/FilterFormTrait.php
@@ -7,21 +7,21 @@ trait FilterFormTrait
     protected function getFilterForm( $filterClass, $selected, $request )
     {
         $filterForm     = $this->createFormBuilder()
-        ->add( 'filterByCategory', EntityType::class, [
-            'class'                 => $filterClass,
-            'choice_label'          => function ( $category ) use ( $request )
-            {
-                return $category->getNameTranslated( $request->getLocale() );
-        },
-        'required'              => true,
-        'label'                 => 'vankosoft_org.form.filter_by_category',
-        'placeholder'           => 'vankosoft_org.form.category_placeholder',
-        'translation_domain'    => 'VankoSoftOrg',
-        'data'                  => $selected ?
-                                    $this->getFilterRepository()->find( $selected ) :
-                                    null,
-        ])
-        ->getForm();
+                            ->add( 'filterByCategory', EntityType::class, [
+                                'class'                 => $filterClass,
+                                'choice_label'          => function ( $category ) use ( $request )
+                                {
+                                    return $category->getNameTranslated( $request->getLocale() );
+                                },
+                                'required'              => true,
+                                'label'                 => 'vs_application.form.filter_by_category',
+                                'placeholder'           => 'vs_application.form.category_placeholder',
+                                'translation_domain'    => 'VSApplicationBundle',
+                                'data'                  => $selected ?
+                                                            $this->getFilterRepository()->find( $selected ) :
+                                                            null,
+                            ])
+                            ->getForm();
         
         return $filterForm;
     }
--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/pages-index.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/pages/pages-index.js
@@ -1,7 +1,17 @@
 require( '../includes/clone_preview.js' );
 require( '../includes/resource-delete.js' );
 
+import { VsPath } from '../includes/fos_js_routes.js';
+
 $( function()
 {
-	
+	$( "#form_filterByCategory" ).on( 'change', function() {
+        let filterCategory  = $( this ).val();
+        let url             = VsPath( 'vs_cms_pages_index' );
+        if ( filterCategory ) {
+            url += filterCategory + '/';
+        }
+        
+        document.location   = url;
+    });
 });
