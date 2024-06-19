eebadc24965d7d8662542fdd2b960e113e5227a4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-06-19 10:25:48 +0000

    Fix Installator to Create Translatable Resources.

--- DataFixtures/AbstractResourceFixture.php
+++ DataFixtures/AbstractResourceFixture.php
@@ -67,8 +67,8 @@ abstract class AbstractResourceFixture implements FixtureInterface
                         continue;
                     }
                     
-                    $resourceCopy           = clone $resource;
-                    $translationResource    = $this->exampleFactory->createTranslation( $resourceCopy, $localeCode, $translationOptions );
+                    //$resourceCopy           = clone $resource;
+                    $translationResource    = $this->exampleFactory->createTranslation( $resource, $localeCode, $translationOptions );
                     
                     if ( $resource instanceof TranslatableInterface ) {
                         if ( $translationResource->getTranslatableLocale() != $resource->getTranslatableLocale() ) {
