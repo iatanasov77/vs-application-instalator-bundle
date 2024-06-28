9f2e8bdc85d58f93d9aacb030eed97543451813b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-06-28 12:10:50 +0000

    Improve ContactForm.

--- src/Vankosoft/ApplicationBundle/Form/ContactForm.php
+++ src/Vankosoft/ApplicationBundle/Form/ContactForm.php
@@ -19,22 +19,26 @@ class ContactForm extends AbstractType
             
             ->add( 'name', TextType::class, [
                 'label'                 => 'vs_application.form.website_contact.name',
-                'translation_domain'    => 'VSApplicationBundle'
+                'translation_domain'    => 'VSApplicationBundle',
+                'attr'                  => ['placeholder' => 'vs_application.form.website_contact.name_placeHolder'],
             ])
             
             ->add( 'email', EmailType::class, [
                 'label'                 => 'vs_application.form.website_contact.email',
-                'translation_domain'    => 'VSApplicationBundle'
+                'translation_domain'    => 'VSApplicationBundle',
+                'attr'                  => ['placeholder' => 'vs_application.form.website_contact.email_placeHolder'],
             ])
             
             ->add( 'subject', TextType::class, [
                 'label'                 => 'vs_application.form.website_contact.subject',
-                'translation_domain'    => 'VSApplicationBundle'
+                'translation_domain'    => 'VSApplicationBundle',
+                'attr'                  => ['placeholder' => 'vs_application.form.website_contact.subject_placeHolder'],
             ])
             
             ->add( 'message', TextareaType::class, [
                 'label'                 => 'vs_application.form.website_contact.message',
-                'translation_domain'    => 'VSApplicationBundle'
+                'translation_domain'    => 'VSApplicationBundle',
+                'attr'                  => ['placeholder' => 'vs_application.form.website_contact.message_placeHolder'],
             ])
             
             ->add( 'btnSubmit', SubmitType::class, [
