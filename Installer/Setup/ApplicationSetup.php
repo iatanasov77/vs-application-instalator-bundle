188730ca7414ec5b5ffe17a69e5504008cc99fb5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-07-22 07:46:39 +0000

    Final Fixes on Current Version Changes.

--- Installer/Setup/ApplicationSetup.php
+++ Installer/Setup/ApplicationSetup.php
@@ -332,30 +332,39 @@ class ApplicationSetup
         $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services.yaml', $configServices );
         
         // Setup Services and Parameters
-        $configServices = str_replace(
-            ["__application_name__", "__application_slug__", "__kernel_class__", "__application_namespace__"],
-            [$this->applicationName, $this->applicationSlug, $this->applicationNamespace . 'Kernel', $this->applicationNamespace],
-            file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/controller.yaml' )
-        );
-        $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/controller.yaml', $configServices );
+        if ( $this->applicationType != AbstractInstallCommand::APPLICATION_TYPE_API ) {
+            $configServices = str_replace(
+                ["__application_name__", "__application_slug__", "__kernel_class__", "__application_namespace__"],
+                [$this->applicationName, $this->applicationSlug, $this->applicationNamespace . 'Kernel', $this->applicationNamespace],
+                file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/controller.yaml' )
+            );
+            $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/controller.yaml', $configServices );
+            
+            // Setup Services and Parameters
+            $configServices = str_replace(
+                ["__application_name__", "__application_slug__", "__kernel_class__", "__application_namespace__"],
+                [$this->applicationName, $this->applicationSlug, $this->applicationNamespace . 'Kernel', $this->applicationNamespace],
+                file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/menu.yaml' )
+            );
+            $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/menu.yaml', $configServices );
+            
+            // Setup Liip Imagine
+            $configLiipImagine  = str_replace(
+                ["__application_slug__"],
+                [$this->applicationSlug],
+                file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/liip_imagine.yaml' )
+            );
+            $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/liip_imagine.yaml', $configLiipImagine );
+        }
         
-        // Setup Services and Parameters
-        $configServices = str_replace(
-            ["__application_name__", "__application_slug__", "__kernel_class__", "__application_namespace__"],
-            [$this->applicationName, $this->applicationSlug, $this->applicationNamespace . 'Kernel', $this->applicationNamespace],
-            file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/menu.yaml' )
-        );
-        $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/services/menu.yaml', $configServices );
-        
-        // Setup Liip Imagine
-        $configLiipImagine  = str_replace(
-            ["__application_slug__"],
-            [$this->applicationSlug],
-            file_get_contents( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/liip_imagine.yaml' )
-        );
-        $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/liip_imagine.yaml', $configLiipImagine );
-        
-        if ( $this->isCatalogProject() || $this->isExtendedProject() ) {
+        if (
+            $this->isCatalogProject() || $this->isExtendedProject() &&
+            (
+                $this->applicationType == AbstractInstallCommand::APPLICATION_TYPE_CATALOG ||
+                $this->applicationType == AbstractInstallCommand::APPLICATION_TYPE_EXTENDED ||
+                $this->applicationType == AbstractInstallCommand::APPLICATION_TYPE_API
+            )
+        ) {
             $configServices = str_replace(
                 [ "__application_namespace__"],
                 [$this->applicationNamespace],
@@ -364,7 +373,13 @@ class ApplicationSetup
             $filesystem->dumpFile( $projectRootDir . '/config/applications/' . $this->applicationSlug . '/packages/vs_payment.yaml', $configServices );
         }
         
-        if ( $this->isExtendedProject() ) {
+        if (
+            $this->isExtendedProject() && 
+            (
+                $this->applicationType == AbstractInstallCommand::APPLICATION_TYPE_EXTENDED ||
+                $this->applicationType == AbstractInstallCommand::APPLICATION_TYPE_API
+            )
+        ) {
             $configServices = str_replace(
                 [ "__application_name__"],
                 [$this->applicationName],
