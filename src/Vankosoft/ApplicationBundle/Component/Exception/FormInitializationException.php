99b82ab579a36e401633e81bc510716766116f35
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-11-10 09:14:31 +0000

    Remove Component: ApplicationBundle

--- src/Vankosoft/ApplicationBundle/Component/Exception/FormInitializationException.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Vankosoft\ApplicationBundle\Component\Exception;
-
-use Exception;
-
-class FormInitializationException extends Exception
-{
-    protected $code = 500;
-}
--- src/Vankosoft/ApplicationBundle/Repository/Interfaces/ApplicationRelationRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Vankosoft\ApplicationBundle\Repository\Interfaces;
-
-use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Vankosoft\ApplicationBundle\Model\Interfaces\ApplicationInterface;
-
-interface ApplicationRelationRepositoryInterface extends RepositoryInterface
-{
-    public function findByApplication( ApplicationInterface $application ) : iterable;
-}
